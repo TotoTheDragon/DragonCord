5524f0602c831cfdb1480828b63551a2c8da8863
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-02-10 16:12:06 +0100

    Fixed issue with ids (used old idea)

--- src/managers/BaseManager.ts
+++ src/managers/BaseManager.ts
@@ -13,7 +13,7 @@ export class BaseManager<K, T extends Base> {
     private readonly _cache: Collection<K, T>;
     private readonly _options: ManagerOptions;
 
-    constructor(client: Client, holds: any, iterable?: Iterable<any>, options: ManagerOptions = {}) {
+    constructor(client: Client, holds: any, iterable?: Iterable<T>, options: ManagerOptions = {}) {
         this._client = client;
 
         if (!Structures) Structures = require("../util/Structures").Structures;
@@ -22,16 +22,18 @@ export class BaseManager<K, T extends Base> {
         this._cache = new Collection(); // Initialize cache
 
         this._options = Util.mergeDefault(DefaultManagerOptions, options);
+
+        if (iterable) for (const i of iterable) this._cache.set(i.valueOf(), i);
     }
 
     add(data: any, cache = true, ...extras: any): T {
         if (data === undefined || data === null) return null;
-        const existing = this._cache.get(data.valueOf());
+        const existing = this._cache.get(data.id);
         if (existing && existing._update && cache && this._options.cache) existing._update(data);
         if (existing) return existing;
 
         const value = new this._holds(this._client, data, ...extras);
-        if (cache && this._options.cache) this._cache.set(data.valueOf(), value);
+        if (cache && this._options.cache) this._cache.set(data.id, value);
         return value;
     }
 
