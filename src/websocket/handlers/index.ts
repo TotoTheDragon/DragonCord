bbcf900ac629d35cc78d8fd8872d2af7443ee777
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-02-10 16:12:31 +0100

    Updateed file to show new events added

--- src/websocket/handlers/index.ts
+++ src/websocket/handlers/index.ts
@@ -1,5 +1,9 @@
+import { guild_create } from "./GUILD_CREATE";
+import { message_create } from "./MESSAGE_CREATE";
 import { ready } from "./READY";
 
 export const handlers = {
-    "READY": ready
+    "READY": ready,
+    "MESSAGE_CREATE": message_create,
+    "GUILD_CREATE": guild_create
 }
