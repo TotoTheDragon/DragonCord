744d3fc7c5e26a07d2d670daad6af44e8afc7e3f
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-02-10 22:54:34 +0100

    Cleared old function call until added back

--- src/websocket/handlers/MESSAGE_CREATE.ts
+++ src/websocket/handlers/MESSAGE_CREATE.ts
@@ -2,6 +2,5 @@ import { Client } from "../../client/Client";
 import { Message } from "../../structure/Message";
 
 export function message_create(client: Client, { d: data }, shard: number) {
-    client.messages.add(data);
     client.emit("message", new Message(client, data));
 }
