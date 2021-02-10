07163f0c0238cb86a4166b912fa4ef24ea1c81c3
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-02-10 15:01:06 +0100

    Created file for Guild structure

---
+++ src/structure/guild/Guild.ts
@@ -0,0 +1,15 @@
+import { Client } from "../../client/Client";
+import { Base } from "../Base";
+
+export class Guild extends Base {
+
+
+
+    constructor(client: Client, data: any) {
+        super(client);
+
+
+
+    }
+
+}
