465992162bbce6783a91a56c6930f7dcf0c8769a
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-02-11 23:37:39 +0100

    Changed request builder to support body

--- src/rest/RequestBuilder.ts
+++ src/rest/RequestBuilder.ts
@@ -4,25 +4,27 @@ const noop = () => { };
 
 const methods = ["get", "post", "patch", "put", "delete"]
 const reflectors = ["toString", "valueOf", "inspect", "constructor"];
-export function routeBuilder(client: Client) {
+export function requestBuilder(client: Client) {
     const route = [''];
+    let method = undefined;
+    let body = undefined;
     const handler = {
         get(target, name) {
+            if (name === "make") return () => client.requestHandler.request(
+                method,
+                route.join("/"),
+                true,
+                body,
+                undefined
+            )
             if (reflectors.includes(name)) return route.join("/");
-            if (methods.includes(name)) {
-                return () => client.requestHandler.request(
-                    name,
-                    route.join("/"),
-                    true,
-                    undefined,
-                    undefined
-                )
-            }
-            route.push(name);
+            if (methods.includes(name)) method = name;
+            else route.push(name);
             return new Proxy(noop, handler);
         },
         apply(target, _, args) {
-            route.push(...args.filter(x => x != null));
+            if (method) body = args[0];
+            else route.push(...args.filter(x => x != null && x != undefined));
             return new Proxy(noop, handler);
         }
     }
