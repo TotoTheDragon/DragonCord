2998af2cdb23d999ec944a08a3e4afff0f4bb817
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-02-11 12:17:12 +0100

    Added debug message to GUILD_CREATE

--- src/websocket/handlers/GUILD_CREATE.ts
+++ src/websocket/handlers/GUILD_CREATE.ts
@@ -1,5 +1,6 @@
 import { Client } from "../../client/Client";
 
 export function guild_create(client: Client, { d: data }, shard: number) {
+    client.logger.emit("DEBUG", "GUILD_CREATE", data.id);
     client.emit("guildCreate", client.guilds.add(data));
 }
