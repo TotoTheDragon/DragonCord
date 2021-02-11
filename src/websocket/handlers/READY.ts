e39087eaa3a020725ab048a38f053dbf84659b31
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-02-11 01:01:36 +0100

    Now store session id on ready event

--- src/websocket/handlers/READY.ts
+++ src/websocket/handlers/READY.ts
@@ -1,5 +1,6 @@
 import { Client } from "../../client/Client";
 
 export function ready(client: Client, { d: data }, shard: number) {
+    client.ws.session = data.session_id;
     client.emit("ready");
 }
