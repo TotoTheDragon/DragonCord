ecc39ba25af378dfcc300fb701803be68a95877b
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-02-11 01:01:25 +0100

    Made some small changes to Client

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,5 +1,6 @@
 
 import { GuildManager } from "../managers/GuildManager";
+import { RequestHandler } from "../rest/RequestHandler";
 import { WebsocketManager } from "../websocket/WebsocketManager";
 import { BaseClient, ClientOptions } from "./BaseClient";
 
@@ -9,15 +10,22 @@ export class Client extends BaseClient {
 
     guilds: GuildManager;
 
+    token: string;
+
+    requestHandler: RequestHandler;
+
     constructor(options: ClientOptions = {}) {
         super(Object.assign({ _tokenType: 'Bot' }, options));
 
         this.ws = new WebsocketManager(this);
 
         this.guilds = new GuildManager(this, undefined, { cache: options.guildCache });
+
+        this.requestHandler = new RequestHandler(this);
     }
 
     async login(token: string): Promise<void> {
+        this.token = token;
         await this.ws.connect();
         this.ws.sendIdentify(token);
     }
