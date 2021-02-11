becbd43e7d13190385e497a679d6b19d7cf6805a
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-02-11 23:40:40 +0100

    Changed debug message for GUILD_CREATE

--- src/websocket/handlers/GUILD_CREATE.ts
+++ src/websocket/handlers/GUILD_CREATE.ts
@@ -1,6 +1,8 @@
 import { Client } from "../../client/Client";
+import { Guild } from "../../structure/guild/Guild";
 
 export function guild_create(client: Client, { d: data }, shard: number) {
-    client.logger.emit("DEBUG", "GUILD_CREATE", data.id);
-    client.emit("guildCreate", client.guilds.add(data));
+    const guild: Guild = client.guilds.add(data)
+    client.logger.emit("DEBUG", "GUILD_CREATE", JSON.stringify(guild.serialize()));
+    client.emit("guildCreate", guild);
 }
