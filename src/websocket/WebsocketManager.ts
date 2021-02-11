f1de42db77ecfd4d43ec3c86e84f6b7f2a853652
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-02-11 01:01:02 +0100

    Cleaned up WebsocketManager

--- src/websocket/WebsocketManager.ts
+++ src/websocket/WebsocketManager.ts
@@ -1,7 +1,6 @@
-import WebSocket from "ws";
-import { BaseClient } from "../client/BaseClient";
-import { Client } from "../client/Client";
 import { EventEmitter } from "events";
+import WebSocket from "ws";
+import { Client } from "../client/Client";
 import { handlers } from "./handlers";
 
 export class WebsocketManager extends EventEmitter {
@@ -10,6 +9,8 @@ export class WebsocketManager extends EventEmitter {
     heartbeatInterval: number;
     lastSequence: number;
     receivedAck: boolean;
+
+    session: string;
 
     client: Client;
 
@@ -26,33 +27,27 @@ export class WebsocketManager extends EventEmitter {
         return new Promise((resolve, reject) => {
             try {
                 this.ws = new WebSocket("wss://gateway.discord.gg/?v=8&encoding=json");
-                this.ws.on("open", () => {
-                    console.log("Websocket opened");
-                });
-                this.ws.on("ping", (data) => console.log(`Ping: `, data));
-                this.ws.on("pong", (data) => console.log(`Pong: `, data));
                 this.ws.on("message", (data) => {
                     const request: DiscordGatewayPayload = JSON.parse(data.toString());
-                    if (request.op === 0) {
-                        if (handlers[request.t]) {
-                            handlers[request.t](this.client, request, this.client.options.shard);
-                        } else console.log(`SHARD #${this.client.options.shard}:`, request);
-                        return;
+                    const { t, op } = request;
+                    switch (op) {
+                        case 0:
+                            if (handlers[t]) handlers[t](this.client, request, this.client.options.shard);
+                            else console.log(`SHARD #${this.client.options.shard}:`, request);
+                            break;
+                        case 10:
+                            console.log("Bot connected");
+                            this.heartbeatInterval = request.d.heartbeat_interval;
+                            this.startHeartbeats();
+                            resolve();
+                            break;
+                        case 11:
+                            this.receivedAck = true;
+                            break;
+                        default:
+                            console.log(request);
+                            break;
                     }
-                    else if (request.op === 10) {
-                        resolve();
-                        console.log("Bot connected");
-                        this.heartbeatInterval = request.d.heartbeat_interval;
-                        this.startHeartbeats();
-                        return;
-                    }
-                    else if (request.op === 11) {
-                        this.receivedAck = true;
-                        return;
-                    } else console.log(request)
-                });
-                this.ws.on("upgrade", (req) => {
-                    console.log(`Upgraded to websocket`);
                 });
             } catch (err) {
                 reject(err);
@@ -88,7 +83,6 @@ export class WebsocketManager extends EventEmitter {
                 }
             )
         )
-
     }
 }
 
