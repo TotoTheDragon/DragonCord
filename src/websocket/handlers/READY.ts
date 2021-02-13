ca89ef25dea6d05d70dcbded35461655f7d0428a
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-02-13 23:39:30 +0100

    Store all guild ids before READY event

--- src/websocket/handlers/READY.ts
+++ src/websocket/handlers/READY.ts
@@ -1,6 +1,11 @@
 import { Client } from "../../client/Client";
+import { ClientUser } from "../../client/ClientUser";
 
 export function ready(client: Client, { d: data }, shard: number) {
+    if (data.guilds)
+        for (const guild of data.guilds)
+            client.guilds.add(guild);
+
     client.ws.session = data.session_id;
     client.emit("ready");
 }
