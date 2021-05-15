810f73ae5a806e9d82309c0d8b32db1a48065f3c
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-05-15 14:41:40 +0200

    Disabled automatically getting channel in message event

--- src/websocket/handlers/MESSAGE_CREATE.ts
+++ src/websocket/handlers/MESSAGE_CREATE.ts
@@ -1,16 +1,14 @@
 import { Client } from "../../client/Client";
-import { APIRequest } from "../../rest/APIRequest";
-import { requestBuilder } from "../../rest/RequestBuilder";
 import { Message } from "../../structure/Message";
 
 export async function message_create(client: Client, { d: data }, shard: number) {
     const message = new Message(client, data)
-    if (client.options.channelCache && message.guild) {
-        const channelRequest: APIRequest = requestBuilder(client)
-            .channels(message.channelID)
-            .get();
-        message.guild.channels.add(await channelRequest.make());
-    }
+    // if (client.options.channelCache && message.guild) {
+    //     const channelRequest: APIRequest = requestBuilder(client)
+    //         .channels(message.channelID)
+    //         .get();
+    //     message.guild.channels.add(await channelRequest.make());
+    // }
     client.logger.silly("Event > MESSAGE", "event", "messageCreate", message.serialize());
     client.emit("message", message);
 }
