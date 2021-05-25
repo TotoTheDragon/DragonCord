a9f7b71d1a2e7524bff6f74cfb3d4759d4f51673
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-05-25 12:04:05 +0200

    Now create new message object on MESSAGE_CREATE

--- src/websocket/handlers/MESSAGE_CREATE.ts
+++ src/websocket/handlers/MESSAGE_CREATE.ts
@@ -1,14 +1,7 @@
 import { Client } from "../../client/Client";
-import { Message } from "../../structure/Message";
 
 export async function message_create(client: Client, { d: data }, shard: number) {
-    const message = new Message(client, data)
-    // if (client.options.channelCache && message.guild) {
-    //     const channelRequest: APIRequest = requestBuilder(client)
-    //         .channels(message.channelID)
-    //         .get();
-    //     message.guild.channels.add(await channelRequest.make());
-    // }
+    const message = client.messages.add(data);
     client.logger.silly("Event > MESSAGE", "event", "messageCreate", message.serialize());
     client.emit("message", message);
 }
