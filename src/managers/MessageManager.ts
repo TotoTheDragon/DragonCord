32690e6e6db641d3ee07ed8734a71ce1e6448ec1
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-05-25 12:02:53 +0200

    Fixed issue where User was used as base in messagemanager

--- src/managers/MessageManager.ts
+++ src/managers/MessageManager.ts
@@ -1,14 +1,13 @@
 import { Client } from "../client/Client";
 import { Message } from "../structure/Message";
 import { PartialCreateOptions, PartialManager } from "../structure/Partial";
-import { User } from "../structure/user/User";
 import { ManagerOptions } from "../util/Constants";
 import { BaseManager } from "./BaseManager";
 
 export class MessageManager extends BaseManager<Message> implements PartialManager {
 
     constructor(client: Client, options?: ManagerOptions) {
-        super(client, User, options);
+        super(client, Message, options);
     }
 
     get(id: string, opts?: PartialCreateOptions): Message {
